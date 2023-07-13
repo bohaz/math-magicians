c4c26195bdaa188d6832d7787bbe2a91fb171b58
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-07-12 20:15:03 -0400

    Add calculator events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import { useState } from 'react';
 import './styles.css';
 import CalculatorTitle from './AppTitle';
+import calculate from '../logic/calculate';
 
 function App() {
+  const [currentOperand, setCurrentOperand] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setCurrentOperand((prevOperand) => calculate(prevOperand, buttonName));
+  };
+
   return (
     <div>
       <CalculatorTitle />
 
       <div className="calculator-grid">
         <div className="output">
-          <div className="current-operand">0</div>
+          <div className="current-operand">{currentOperand.next || currentOperand.total || '0'}</div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange">*</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange">=</button>
+        <button type="button" onClick={() => handleClick('AC')}>AC</button>
+        <button type="button" onClick={() => handleClick('+/-')}>+/-</button>
+        <button type="button" onClick={() => handleClick('%')}>%</button>
+        <button type="button" className="orange" onClick={() => handleClick('÷')}>÷</button>
+        <button type="button" onClick={() => handleClick('7')}>7</button>
+        <button type="button" onClick={() => handleClick('8')}>8</button>
+        <button type="button" onClick={() => handleClick('9')}>9</button>
+        <button type="button" className="orange" onClick={() => handleClick('x')}>x</button>
+        <button type="button" onClick={() => handleClick('4')}>4</button>
+        <button type="button" onClick={() => handleClick('5')}>5</button>
+        <button type="button" onClick={() => handleClick('6')}>6</button>
+        <button type="button" className="orange" onClick={() => handleClick('-')}>-</button>
+        <button type="button" onClick={() => handleClick('1')}>1</button>
+        <button type="button" onClick={() => handleClick('2')}>2</button>
+        <button type="button" onClick={() => handleClick('3')}>3</button>
+        <button type="button" className="orange" onClick={() => handleClick('+')}>+</button>
+        <button type="button" className="span-two" onClick={() => handleClick('0')}>0</button>
+        <button type="button" onClick={() => handleClick('.')}>.</button>
+        <button type="button" className="orange" onClick={() => handleClick('=')}>=</button>
       </div>
 
     </div>
